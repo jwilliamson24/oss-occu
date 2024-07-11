7a499f67776925e989b1e2617ae8957df5ce2150
jwilliamson24 <jasmine.m.williamson@gmail.com>
2024-07-11 12:07:57 -0700

    enes nimble model, need to fix convrgence

--- code/06-rework-nimble-models.R
+++ code/06-rework-nimble-models.R
@@ -22,7 +22,17 @@ library(MCMCvis)
 library(boot)
 source('attach.nimble_v2.R')
 
-load("./all.spp_model_2.RData")
+
+## saved model outputs -----------------------------------------------------------------------------------
+
+# all species model:
+# load("./all.spp_model_2.RData")
+
+# oss model:
+# load("./oss_model.RData")
+
+# enes model:
+# load("./enes_model.RData")
 
 
 ## load Data--------------------------------------------------------------------------------------------------
@@ -105,7 +115,7 @@ table(data2$treatment)
 scaled_temp <- c(scale(data$temp))
 
 
-## Build Model------------------------------------------------------------------------------------------------
+## ALL SPP MODEL ------------------------------------------------------------------------------------------------
 
 all.spp.model.2 <- nimbleCode ({
   
@@ -176,7 +186,7 @@ save(mcmc.output.2, file="./all.spp_model_2.RData")
 load("./all.spp_model_2.RData")
 
 
-## Assessing Convergence------------------------------------------------------------------------------------------------------
+## assessing convergence------------------------------------------------------------------------------------------------------
 #mcmcplot(mcmc.output.1$samples)
 
 # Gelman-Rubin diagnostic (AKA RHat or PSRF)
@@ -188,7 +198,7 @@ PSRF <- bind_cols(colnames(z$chain1),g) %>% rename(Parameter = ...1 ,PSRF = ...2
 PSRF # Values are below 1.05, so that's good
 
 
-## Interpreting Model Outputs----------------------------------------------------------------------------------------------------
+## interpreting model outputs----------------------------------------------------------------------------------------------------
 #z
 
 # Inverse logit the detection intercept to get detection probabilities
@@ -272,5 +282,347 @@ ggsave(filename = "boxplot_trt_occu_prob_nimble.png", plot = p, device = "png",
        width = 10, height = 6, units = "in", dpi = 300)
 
 
+## OSS MODEL ------------------------------------------------------------------------------------------------
+
+oss.model <- nimbleCode ({
+  
+  # Priors 
+  # uninformative, vague priors
+  
+  for(t in 1:n.treatments){
+    TreatmentIntercept[t] ~ dunif(-10,10)
+  }#t
+  
+  DetectionIntercept ~ dunif(-5,5)
+  betaTemp ~ dunif(-5, 5)
+  betaTemp2 ~ dunif(-5, 0)
+  
+  # Likelihood
+  
+  # Process/Biological model = Occupancy
+  # need two pieces: one defining psi(occu prob coeff) and covariates, and one defining z dist
+  for(i in 1:n.sites) {
+    logit(psi[i]) <- TreatmentIntercept[treatment[i]]  #psi=occupancy probability
+    z[i] ~ dbern(psi[i])  # z=1 if occupied, z=latent true occupancy
+  }#i
+  
+  # Observation model = Detection
+  # need two pieces: one for p(det prob coeff) and one defining Y distribution
+  for(j in 1:n.obs) {
+    logit(p[j]) <- DetectionIntercept + betaTemp*temp[j] + betaTemp2*temp[j]^2 
+    #using temp as covariate with a quadratic relationship
+    #p=detection probability for site i and survey j
+    Y[j] ~ dbern(p[j] * z[site[j]]) #Y=my actual data observations
+    #z=1 or 0, turns this on or off
+  }#j
+  
+})
+
+# Parameters monitored
+parameters <- c("z","p","TreatmentIntercept","DetectionIntercept","betaTemp", "betaTemp2")
+
+# MCMC Settings
+ni <- 40000
+nt <- 40
+nb <- 20000
+nc <- 3
+
+# Data
+nimble.data = list(Y=data$oss.obs,
+                   temp=scaled_temp)
+
+nimble.constants = list(n.sites = length(unique(data$site)),
+                        n.treatments = length(unique(data2$treatment)),
+                        treatment=data2$treatment,
+                        site=as.numeric(as.factor(data$site)),
+                        n.obs = length(data$all.obs))
+
+mcmc.output.3 <- nimbleMCMC(code = oss.model,
+                            data = nimble.data,
+                            constants=nimble.constants,
+                            monitors = parameters,
+                            niter = ni,
+                            nburnin = nb,
+                            nchains = nc,
+                            thin=nt,
+                            summary=TRUE,
+                            samplesAsCodaMCMC = TRUE)
+
+attach.nimble(mcmc.output.3$samples)
+save(mcmc.output.3, file="./oss_model.RData")
+load("./oss_model.RData")
+
+
+## assessing convergence------------------------------------------------------------------------------------------------------
+#mcmcplot(mcmc.output.1$samples)
+
+# Gelman-Rubin diagnostic (AKA RHat or PSRF)
+z <- mcmc.output.3$samples
+g <- matrix(NA, nrow=nvar(z), ncol=2)
+for (v in 1:nvar(z)) { g[v,] <- gelman.diag(z[,v])$psrf }
+PSRF <- bind_cols(colnames(z$chain1),g) %>% rename(Parameter = ...1 ,PSRF = ...2 ,PSRFUpperCI = ...3) 
+
+PSRF # Values are below 1.05, so that's good
+
+
+## interpreting model outputs----------------------------------------------------------------------------------------------------
+#z
+
+# Inverse logit the detection intercept to get detection probabilities
+det.probs.inv <- inv.logit(DetectionIntercept)
+hist(det.probs.inv)
+
+# Looking at trace plots and parameter estimates
+MCMCtrace(object = mcmc.output.3$samples,
+          pdf = FALSE, # no export to PDF
+          ind = TRUE, # separate density lines per chain
+          params = c("DetectionIntercept", "betaTemp", "TreatmentIntercept"))
+#this looks good:
+#caterpillars on traceplot are mostly lined up/overlapping
+#parameter estimate lines on density plot are mostly overlapping
+
+
+mean(det.probs.inv) # = 0.26108
+mean(det.probs.inv>0)  # = 1
+median(det.probs.inv)  # = 0.2597028
+boxplot(det.probs.inv)
+
+# Inv logit TreatmentIntercept to get Occupancy Estimates
+trt.int.inv <- inv.logit(TreatmentIntercept)
+median(trt.int.inv[,1]) # 0.6392119    BS
+median(trt.int.inv[,2]) # 0.8090534    BU
+median(trt.int.inv[,3]) # 0.5784290    HB
+median(trt.int.inv[,4]) # 0.6915276    HU
+median(trt.int.inv[,5]) # 0.9920444    UU
+
+
+
+## base R boxplot for treatment effect -----------------------------------------------------------------------------------------------
+
+# Renaming and reordering the treatment intercepts for the boxplot
+# treatment_matrix <- TreatmentIntercept
+trt.int.inv <- inv.logit(TreatmentIntercept)
+treatment_matrix <- trt.int.inv # Using the inv logit treatment estimates
+
+new.names <- c("Salvage Logged", "Wildfire", "Harvest, Wildfire", "Harvest", "Control")
+colnames(treatment_matrix) <- new.names
+desired.order <- c("Control", "Wildfire", "Harvest, Wildfire", "Harvest", "Salvage Logged")
+
+box.colors <- c('lightgreen','steelblue', 'coral2', '#f9d62e', '#b967ff' )
+
+#boxplot of Treatment Estimates - occu prob for each treatment
+boxplot(treatment_matrix[, match(desired.order, colnames(treatment_matrix))], 
+        main = "Treatment Intercepts for All Species", 
+        xlab = "Treatment", ylab = "Occupancy Probability",
+        col = box.colors)
+
+
+## ggplot boxplot treatment effect -----------------------------------------------------------------------------------------------
+
+#define treatments using the inv logit treatment estimates
+treatment_matrix <- trt.int.inv 
+#treatments are numbered, need to give them names
+new.names <- c("Salvage Logged", "Wildfire", "Harvest, Wildfire", "Harvest", "Control") 
+colnames(treatment_matrix) <- new.names
+
+
+treatment_matrix <- as.data.frame(treatment_matrix)
+#reshape data to long format bc ggplot required
+long_format <- gather(treatment_matrix, key = "Treatment", value = "Occupancy_Probability")
+
+#specify treatment order for plot
+long_format$Treatment <- factor(long_format$Treatment, 
+                                levels = c("Control", "Wildfire", "Harvest, Wildfire", "Harvest", "Salvage Logged")) # Replace "DesiredOrder", "Treat1", etc., with your actual treatment names in the desired order
+
+
+box.colors <- c('lightgreen','steelblue', 'coral2', '#f9d62e', '#b967ff' )
+
+p <- ggplot(long_format, aes(x = Treatment, y = Occupancy_Probability, fill = Treatment)) +
+  geom_boxplot() +
+  theme_classic() +
+  scale_fill_manual(values = box.colors) +
+  labs(title = "Treatment Intercepts for OSS", x = "Treatment", y = "Occupancy Probability") +
+  theme(plot.title = element_text(hjust = 0.5)) # Center the title
+
+ggsave(filename = "oss_trt_occu_prob.png", plot = p, device = "png", 
+       path = "C:/Users/jasmi/OneDrive/Documents/Academic/OSU/Git/oss-occu/figures/06-rework-nimble-models",
+       width = 10, height = 6, units = "in", dpi = 300)
+
+
+## ENES MODEL ------------------------------------------------------------------------------------------------
+
+enes.model <- nimbleCode ({
+  
+  # Priors 
+  # uninformative, vague priors
+  
+  for(t in 1:n.treatments){
+    TreatmentIntercept[t] ~ dunif(-10,10)
+  }#t
+  
+  DetectionIntercept ~ dunif(-5,5)
+  betaTemp ~ dunif(-5, 5)
+  betaTemp2 ~ dunif(-5, 0)
+  
+  # Likelihood
+  
+  # Process/Biological model = Occupancy
+  # need two pieces: one defining psi(occu prob coeff) and covariates, and one defining z dist
+  for(i in 1:n.sites) {
+    logit(psi[i]) <- TreatmentIntercept[treatment[i]]  #psi=occupancy probability
+    z[i] ~ dbern(psi[i])  # z=1 if occupied, z=latent true occupancy
+  }#i
+  
+  # Observation model = Detection
+  # need two pieces: one for p(det prob coeff) and one defining Y distribution
+  for(j in 1:n.obs) {
+    logit(p[j]) <- DetectionIntercept + betaTemp*temp[j] + betaTemp2*temp[j]^2 
+    #using temp as covariate with a quadratic relationship
+    #p=detection probability for site i and survey j
+    Y[j] ~ dbern(p[j] * z[site[j]]) #Y=my actual data observations
+    #z=1 or 0, turns this on or off
+  }#j
+  
+})
+
+# Parameters monitored
+parameters <- c("z","p","TreatmentIntercept","DetectionIntercept","betaTemp", "betaTemp2")
+
+# MCMC Settings
+ni <- 80000
+nt <- 80
+nb <- 40000
+nc <- 3
+
+# Data
+nimble.data = list(Y=data$enes.obs,
+                   temp=scaled_temp)
+
+nimble.constants = list(n.sites = length(unique(data$site)),
+                        n.treatments = length(unique(data2$treatment)),
+                        treatment=data2$treatment,
+                        site=as.numeric(as.factor(data$site)),
+                        n.obs = length(data$all.obs))
+
+mcmc.output.4 <- nimbleMCMC(code = enes.model,
+                            data = nimble.data,
+                            constants=nimble.constants,
+                            monitors = parameters,
+                            niter = ni,
+                            nburnin = nb,
+                            nchains = nc,
+                            thin=nt,
+                            summary=TRUE,
+                            samplesAsCodaMCMC = TRUE)
+
+#######     warning: logProb of data node Y[724]: logProb is -Inf. what does this mean??
+
+
+attach.nimble(mcmc.output.4$samples)
+save(mcmc.output.4, file="./enes_model.RData")
+load("./enes_model.RData")
+
+
+## Assessing Convergence------------------------------------------------------------------------------------------------------
+
+mcmcplot(mcmc.output.4$samples)
+
+# mcmc plots dont look great but I'm not really sure what to do with them....
+
+# Gelman-Rubin diagnostic (AKA RHat or PSRF)
+z <- mcmc.output.4$samples
+g <- matrix(NA, nrow=nvar(z), ncol=2)
+for (v in 1:nvar(z)) { g[v,] <- gelman.diag(z[,v])$psrf }
+PSRF <- bind_cols(colnames(z$chain1),g) %>% rename(Parameter = ...1 ,PSRF = ...2 ,PSRFUpperCI = ...3) 
+
+PSRF # some values were 1.05 and above, but i dont know what that means. 
+# fixed that after I changed iterations from 40000 to 80000
+
+# ACF plots
+acf(DetectionIntercept)
+acf(TreatmentIntercept)
+
+## Interpreting Model Outputs----------------------------------------------------------------------------------------------------
+#z
+
+# Inverse logit the detection intercept to get detection probabilities
+det.probs.inv <- inv.logit(DetectionIntercept)
+hist(det.probs.inv)
+
+# Looking at trace plots and parameter estimates
+MCMCtrace(object = mcmc.output.4$samples,
+          pdf = FALSE, # no export to PDF
+          ind = TRUE, # separate density lines per chain
+          params = c("DetectionIntercept", "betaTemp", "TreatmentIntercept"))
+#this looks iffy:
+#caterpillars on traceplot are looking pretty varied
+#though parameter estimate lines on density plot are mostly overlapping
+#changed nt from 40 to 80 and nburn from 20000 to 40000 and it didnt fix that
+
+
+mean(det.probs.inv) # = 0.2403386
+mean(det.probs.inv>0)  # = 1
+median(det.probs.inv)  # = 0.2394667
+boxplot(det.probs.inv)
+
+# Inv logit TreatmentIntercept to get Occupancy Estimates
+trt.int.inv <- inv.logit(TreatmentIntercept)
+median(trt.int.inv[,1]) # 0.6271816    BS
+median(trt.int.inv[,2]) # 0.5330411    BU
+median(trt.int.inv[,3]) # 0.4669251    HB
+median(trt.int.inv[,4]) # 0.5864338    HU
+median(trt.int.inv[,5]) # 0.5549356    UU
+
+
+
+## base R boxplot for treatment effect -----------------------------------------------------------------------------------------------
+
+# Renaming and reordering the treatment intercepts for the boxplot
+trt.int.inv <- inv.logit(TreatmentIntercept)
+treatment_matrix <- trt.int.inv # Using the inv logit treatment estimates
+
+new.names <- c("Salvage Logged", "Wildfire", "Harvest, Wildfire", "Harvest", "Control")
+colnames(treatment_matrix) <- new.names
+desired.order <- c("Control", "Wildfire", "Harvest, Wildfire", "Harvest", "Salvage Logged")
+
+box.colors <- c('lightgreen','steelblue', 'coral2', '#f9d62e', '#b967ff' )
+
+#boxplot of Treatment Estimates - occu prob for each treatment
+boxplot(treatment_matrix[, match(desired.order, colnames(treatment_matrix))], 
+        main = "Treatment Intercepts for All Species", 
+        xlab = "Treatment", ylab = "Occupancy Probability",
+        col = box.colors)
+
+
+## ggplot boxplot treatment effect -----------------------------------------------------------------------------------------------
+
+#define treatments using the inv logit treatment estimates
+treatment_matrix <- trt.int.inv 
+#treatments are numbered, need to give them names
+new.names <- c("Salvage Logged", "Wildfire", "Harvest, Wildfire", "Harvest", "Control") 
+colnames(treatment_matrix) <- new.names
+
+
+treatment_matrix <- as.data.frame(treatment_matrix)
+#reshape data to long format bc ggplot required
+long_format <- gather(treatment_matrix, key = "Treatment", value = "Occupancy_Probability")
+
+#specify treatment order for plot
+long_format$Treatment <- factor(long_format$Treatment, 
+                                levels = c("Control", "Wildfire", "Harvest, Wildfire", "Harvest", "Salvage Logged")) # Replace "DesiredOrder", "Treat1", etc., with your actual treatment names in the desired order
+
+
+box.colors <- c('lightgreen','steelblue', 'coral2', '#f9d62e', '#b967ff' )
+
+p <- ggplot(long_format, aes(x = Treatment, y = Occupancy_Probability, fill = Treatment)) +
+  geom_boxplot() +
+  theme_classic() +
+  scale_fill_manual(values = box.colors) +
+  labs(title = "Treatment Intercepts for ENES", x = "Treatment", y = "Occupancy Probability") +
+  theme(plot.title = element_text(hjust = 0.5)) # Center the title
+
+ggsave(filename = "enes_trt_occu_prob.png", plot = p, device = "png", 
+       path = "C:/Users/jasmi/OneDrive/Documents/Academic/OSU/Git/oss-occu/figures/06-rework-nimble-models",
+       width = 10, height = 6, units = "in", dpi = 300)
 
 
